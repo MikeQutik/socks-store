e4a7c920672aa83b19c27bf8587a31cb0d3ad6cf
GMGinIT <gmginit@gmail.com>
2025-01-30 12:30:19 +0300

    newnav

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Navigation from "../widgets/Navigation/Navigation";
 import React from "react";
-// import "./App.css";
+import "./App.css";
 // import Logo from "../shared/ui/logo/logo";
 import HomePage from "../pages/HomePage/HomePage";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
